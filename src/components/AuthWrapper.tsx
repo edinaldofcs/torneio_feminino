0248580b83b63221c64febc76d341b9bec1df108
edinaldofcs <edinaldofcs@gmail.com>
2025-07-01 19:07:42 -0300

    style: ajuste de cores

--- src/components/AuthWrapper.tsx
+++ src/components/AuthWrapper.tsx
@@ -29,19 +29,19 @@ export default function AuthWrapper({ children }: { children: React.ReactNode })
     return (
       <div className="flex items-center justify-center h-screen">
         <div className="absolute inset-0 bg-[url('/logo.png')] bg-cover bg-center -z-2" />
-        <div className="bg-black p-6 rounded-xl shadow-xl w-full max-w-sm border-4 border-gray-400">
-          <h1 className="text-xl font-semibold mb-4 text-center text-white">
+        <div className="bg-white p-6 rounded-xl shadow-xl w-full max-w-sm border-4 border-pink-400">
+          <h1 className="text-xl font-semibold mb-4 text-center text-pink-600">
             Controle de Acesso – Informe sua senha
           </h1>
           <input
             type="password"
             placeholder="Digite a senha"
-            className="w-full p-2 border border-gray-300 rounded mb-4 text-white"
+            className="w-full p-2 border border-pink-300 rounded mb-4 text-black"
             value={senha}
             onChange={(e) => setSenha(e.target.value)}
           />
           <button
-            className="w-full bg-blue-600 text-white p-2 rounded hover:bg-blue-700"
+            className="w-full bg-pink-600 text-white p-2 rounded hover:bg-pink-700"
             onClick={handleLogin}
           >
             Entrar
